37e8455d55b5d8e8a3bcaa979bbcaf2897746b19
kimlong <kimlong5244@gmail.com>
2025-06-03 08:34:20 +0700

    update SEO for telegram

--- src/app/(client)/preview/page.tsx
+++ src/app/(client)/preview/page.tsx
@@ -1,5 +1,11 @@
 import { ConfessionPreview } from "@/components/oragnisms/confession-preview";
+import { Metadata } from "next";
 import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Sabay Konfess",
+};
+
 export default async function Page() {
   return (
     <Suspense>
