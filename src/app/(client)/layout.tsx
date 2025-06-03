28d02327329266ee118600d4c5eedf3c78f5b699
kimlong <kimlong5244@gmail.com>
2025-06-03 08:38:20 +0700

    update SEO for telegram

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,8 +1,5 @@
-// app/layout.tsx
-import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { headers } from "next/headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,49 +11,38 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export async function generateMetadata(): Promise<Metadata> {
-  const userAgent = (await headers()).get("user-agent") || "";
-  const isTelegramBot = userAgent.includes("TelegramBot");
-
-  if (isTelegramBot) {
-    return {
-      title: "Sabay Konfess",
-    };
-  }
-
-  return {
+export const metadata: Metadata = {
+  title: "Sabay Konfess",
+  description:
+    "A safe space to express your deepest thoughts, secrets, and confessions. No judgment, no names, just pure honesty.",
+  keywords: ["Confession", "Sabay Konfess"],
+  authors: [{ name: "Kimlong Chann", url: "https://kimlongchann.dev" }],
+  creator: "Kimlong Chann",
+  publisher: "Kimlong Chann",
+  openGraph: {
     title: "Sabay Konfess",
     description:
       "A safe space to express your deepest thoughts, secrets, and confessions. No judgment, no names, just pure honesty.",
-    keywords: ["Confession", "Sabay Konfess"],
-    authors: [{ name: "Kimlong Chann", url: "https://kimlongchann.dev" }],
-    creator: "Kimlong Chann",
-    publisher: "Kimlong Chann",
-    openGraph: {
-      title: "Sabay Konfess",
-      description:
-        "A safe space to express your deepest thoughts, secrets, and confessions. No judgment, no names, just pure honesty.",
-      url: "https://sabaykonfess.kimlongchann.dev",
-      siteName: "Sabay Konfess",
-      type: "website",
-      images: [
-        {
-          url: "https://sabaykonfess.kimlongchann.dev/images/og-image.png",
-          width: 1200,
-          height: 630,
-          alt: "Sabay Konfess",
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: "Kimlong Chann - Full Stack Developer",
-      description:
-        "Hi there, I am Kimlong Chann, a full stack developer specializing in building modern web applications using React, Node.js, Laravel, and more.",
-      images: ["https://sabaykonfess.kimlongchann.dev/images/og-image.png"],
-    },
-  };
-}
+    url: "https://sabaykonfess.kimlongchann.dev",
+    siteName: "Sabay Konfess",
+    type: "website",
+    images: [
+      {
+        url: "https://sabaykonfess.kimlongchann.dev/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Sabay Konfess",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Kimlong Chann - Full Stack Developer",
+    description:
+      "Hi there, I am Kimlong Chann, a full stack developer specializing in building modern web applications using React, Node.js, Laravel, and more.",
+    images: ["https://sabaykonfess.kimlongchann.dev/images/og-image.png"],
+  },
+};
 
 export default function RootLayout({
   children,
